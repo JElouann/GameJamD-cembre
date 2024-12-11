1fcd37cd3c809b034675e11edcc0c63d2f942c50
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2024-12-11 16:54:45 +0100

    FIN

--- GameJamDecembre/Assets/Scripts/Player/PlayerController.cs
+++ GameJamDecembre/Assets/Scripts/Player/PlayerController.cs
@@ -1,24 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private Vector2 _movement;
-    [SerializeField] private float _speed;
+    private float _speed;
+
     [SerializeField]
+    private Vector2 _movement;
+
     private Rigidbody _rb;
+    public Transform cameraTransform; // Assign your camera transform here
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         _movement = context.ReadValue<Vector2>();
     }
-
-    private void Update()
+    private void FixedUpdate()
     {
-        Vector3 truc = new Vector3(_movement.x, 0, _movement.y) * _speed * Time.deltaTime;
-        transform.Translate(truc);
+        if (_rb != null && cameraTransform != null)
+        {
+            // Get the camera's forward and right directions
+            Vector3 forward = cameraTransform.forward;
+            Vector3 right = cameraTransform.right;
+
+            // Flatten the directions to ignore vertical movement
+            forward.y = 0f;
+            right.y = 0f;
+
+            forward.Normalize();
+            right.Normalize();
+
+            // Calculate movement direction relative to the camera
+            Vector3 movement = (forward * _movement.y + right * _movement.x) * _speed * Time.fixedDeltaTime;
+            _rb.MovePosition(_rb.position + movement);
+        }
     }
 }
