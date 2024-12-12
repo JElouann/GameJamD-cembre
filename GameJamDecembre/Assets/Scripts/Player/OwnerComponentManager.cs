0c93011fe231211a8a2646b763bddf90c35b7fa2
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2024-12-12 16:00:39 +0100

    aZQZERDGHJ

--- GameJamDecembre/Assets/Scripts/Player/OwnerComponentManager.cs
+++ GameJamDecembre/Assets/Scripts/Player/OwnerComponentManager.cs
@@ -3,12 +3,17 @@ using Unity.Netcode;
 
 public class OwnerComponentManager : NetworkBehaviour
 {
-    [SerializeField] private Camera _camera; // This is your camera, assign it in the prefab
+    [SerializeField] 
+    private Camera _camera; // This is your camera, assign it in the prefab
+
+    [SerializeField] 
+    private GameObject _panel;
 
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         if (!IsOwner) { return; } // ALL players will read this method, only player owner will execute past this line
         _camera.enabled = true; // only enable YOUR PLAYER'S camera, all others will stay disabled
+        _panel.SetActive(true);
     }
 }
