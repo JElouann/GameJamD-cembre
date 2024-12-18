afbad9f3f860218eeb51308c0580c2232949695a
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2024-12-18 15:22:03 +0100

    azerhjklm

--- GameJamDecembre/Assets/Scripts/Multi/ColliderTresVicieuse.cs
+++ GameJamDecembre/Assets/Scripts/Multi/ColliderTresVicieuse.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.Netcode;
@@ -6,72 +5,61 @@ using UnityEngine;
 
 public class ColliderTresVicieuse : NetworkBehaviour
 {
-    //Les 6 id de joueur
-    [SerializeField] private List<ulong> _idPlayersInGame;
+    private const int PlayerLayer = 7;
 
-    //Les scoreds des 6 joueurs
-    [SerializeField] private NetworkVariable<int> _scoreJ1 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-    [SerializeField] private NetworkVariable<int> _scoreJ2 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-    [SerializeField] private NetworkVariable<int> _scoreJ3 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-    [SerializeField] private NetworkVariable<int> _scoreJ4 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-    [SerializeField] private NetworkVariable<int> _scoreJ5 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-    [SerializeField] private NetworkVariable<int> _scoreJ6 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    [SerializeField] private List<ulong> _idPlayersInGame = new List<ulong>();
+    private Dictionary<ulong, NetworkVariable<int>> _playerScores = new Dictionary<ulong, NetworkVariable<int>>();
 
     [SerializeField] private TextMeshProUGUI _loiEnCours;
 
-
-    /// <summary>
-    /// Trigger très coquin qui pirate les données du joueur
-    /// </summary>
-    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
-        if (IsOwner)
+        if (IsOwner && other.gameObject.layer == PlayerLayer)
         {
-            if (other.gameObject.layer == 7) //Si c'est un player
+            var playerComponent = other.gameObject.GetComponent<OwnerComponentManager>();
+            if (playerComponent != null)
             {
-                print(other.gameObject);
-                print(other.gameObject.GetComponent<OwnerComponentManager>());
-                print(other.gameObject.GetComponent<OwnerComponentManager>().IdPlayer);
-                DownloadId(other.gameObject.GetComponent<OwnerComponentManager>().IdPlayer);
+                RegisterPlayerId(playerComponent.IdPlayer);
             }
         }
-
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer == 7) //Si c'est un player
+        if (other.gameObject.layer == PlayerLayer)
         {
-            _loiEnCours.text = "Prenez place pour parler de votre loi"; //On invite un autre joueur à venir
+            _loiEnCours.text = "Prenez place pour parler de votre loi";
         }
     }
 
-    /// <summary>
-    /// On enregistre potentiellement un nouvelle id
-    /// </summary>
-    /// <param name="_idPotential"></param>
-    private void DownloadId(ulong _idPotential)
+    private void RegisterPlayerId(ulong playerId)
     {
-        foreach (ulong _id in _idPlayersInGame) //parmis tout les id
+        if (!_idPlayersInGame.Contains(playerId))
         {
-            if (_idPotential == _id) //Si je l'ai déjà enregistré
+            print("ajout");
+            _idPlayersInGame.Add(playerId);
+            if (!_playerScores.ContainsKey(playerId))
             {
-                AfficheLoiPlayer(_idPotential);
+                _playerScores[playerId] = new NetworkVariable<int>(0);
             }
         }
-        _idPlayersInGame.Add(_idPotential); //On enregister l'id du joueur car on ne le connaît pas
-        AfficheLoiPlayer(_idPotential);
+        DisplayPlayerLaw(playerId);
     }
 
-    /// <summary>
-    /// On récupert les infos liés au l'id
-    /// </summary>
-    /// <param name="_idPlayer"></param>
-    private void AfficheLoiPlayer(ulong _idPlayer)
+    private void DisplayPlayerLaw(ulong playerId)
     {
-        GameObject _playerDownload = NetworkManager.Singleton.ConnectedClients[_idPlayer].PlayerObject.gameObject;
-        _loiEnCours.text = _playerDownload.GetComponent<InputLoi>().ValueLaws.Value.ToString();
-
+        if (NetworkManager.Singleton.ConnectedClients.TryGetValue(playerId, out var client))
+        {
+            GameObject playerObject = client.PlayerObject.gameObject;
+            var inputLoi = playerObject.GetComponent<InputLoi>();
+            if (inputLoi != null)
+            {
+                _loiEnCours.text = inputLoi.ValueLaws.Value.ToString();
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"Player ID {playerId} not found.");
+        }
     }
 }
