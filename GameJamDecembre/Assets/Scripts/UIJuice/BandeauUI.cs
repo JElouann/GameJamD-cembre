236ba7c705e62eb95d9f7e52041936774ade7a37
JElouann <elouann.jouenne@e-artsup.net>
2024-12-19 11:39:59 +0100

    singleton du bandeau

--- GameJamDecembre/Assets/Scripts/UIJuice/BandeauUI.cs
+++ GameJamDecembre/Assets/Scripts/UIJuice/BandeauUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BandeauUI : MonoBehaviour
@@ -11,7 +12,39 @@ public class BandeauUI : MonoBehaviour
     [SerializeField] private List<PathStep> _pathSteps = new();
     private int _index = 0;
 
-    public void UpdateDisplayedText(string content)
+    // Singleton
+    #region Singleton
+    private static BandeauUI _instance;
+
+    public static BandeauUI Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("ça n'arrivera pas");
+                _instance = go.AddComponent<BandeauUI>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(this.gameObject);
+            Debug.Log($"<b><color=#{UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0f, 0f, 1f, 1f).ToHexString()}>{this.GetType()}</color> instance <color=#eb624d>destroyed</color></b>");
+        }
+        else
+        {
+            _instance = this;
+            Debug.Log($"<b><color=#{UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f, 1f, 1f).ToHexString()}>{this.GetType()}</color> instance <color=#58ed7d>created</color></b>");
+        }
+    }
+    #endregion
+
+    public void ShowText(string content)
     {
         _text.text = content;
         Reset();
